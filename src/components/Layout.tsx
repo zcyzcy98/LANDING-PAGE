3b965f236d7299ec79d64f6bdf6e1fb913b332e9
zcyzcy98 <51557777+zcyzcy98@users.noreply.github.com>
2025-12-09 16:37:23 +0800

    feat(layout): 新增logo

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import { Navbar } from "./elements/Navbar";
+import { Footer } from "./elements/Footer";
+
+interface LayoutProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+export default function Layout({ title, children }: LayoutProps) {
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+  return (
+    <div>
+      <Navbar />
+      <main className="flex flex-col gap-y-20 md:gap-y-32 overflow-hidden">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
