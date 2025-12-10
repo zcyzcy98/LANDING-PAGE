7456dfb214e4d066107110d73fe0affa95e303fb
zcyzcy98 <51557777+zcyzcy98@users.noreply.github.com>
2025-12-10 11:51:43 +0800

    feat(alins):增加别名@

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path"; // 引入 path 模块
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // 关键代码：将 '@' 映射到 'src' 目录
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
