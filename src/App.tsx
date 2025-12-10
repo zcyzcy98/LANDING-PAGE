d089ee650c5bcf07db22a31ffcb16e6f77195582
zcyzcy98 <51557777+zcyzcy98@users.noreply.github.com>
2025-12-10 15:19:36 +0800

    feat(CTA)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Hero } from "@/components/sections/Hero";
 import { Services } from "@/components/sections/Services";
 import { AboutUs } from "@/components/sections/AboutUs";
 import { Pricing } from "@/components/sections/Pricing";
+import { CTA } from "@/components/sections/CallToAction";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Services />
         <AboutUs />
         <Pricing />
+        <CTA />
       </Layout>
     </>
   );
---
+++ src/components/elements/Footer.tsx
@@ -0,0 +1,3 @@
+export const Footer = () => {
+  return <footer className="relative pt-28 rounded-t-3xl bg-box-bg"></footer>;
+};
